18c029a32a2912a72702de723e38b634b1b32e3e
jeeves-solutions <jeeves.anderson@gmail.com>
2017-04-23 22:32:51 +0100

    Factory for Basic HTTP Client

--- src/main/java/solutions/jeeves/ircbot/http/client/BasicHttpClientFactory.kt
+++ src/main/java/solutions/jeeves/ircbot/http/client/BasicHttpClientFactory.kt
@@ -1,5 +1,15 @@
 package solutions.jeeves.ircbot.http.client
 
-interface BasicHttpClientFactory {
-    fun get(): BasicHttpClient
+import org.apache.http.impl.client.BasicCookieStore
+import org.apache.http.impl.client.HttpClients
+import org.springframework.stereotype.Component
+
+@Component
+open class BasicHttpClientFactory {
+
+    fun get(): BasicHttpClient = BasicHttpClientImpl(getClient())
+
+    private fun getClient() = HttpClients.custom()
+            .setDefaultCookieStore(BasicCookieStore())
+            .build()
 }
